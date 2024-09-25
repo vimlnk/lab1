faa3bd53c418f9c91135a75d4cb7ab871ffc3f77
Admin <melnukvikysa@gmail.com>
2024-09-25 20:48:40 +0300

    Добавьте файлы проекта.

---
+++ lab1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using lab1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab1.Controllers
+{
+	public class HomeController : Controller
+	{
+		// Метод для обробки запиту на головну сторінку
+		public IActionResult Index()
+		{
+			// Передаємо дані через ViewBag
+			ViewBag.Name = "Мельник Вікторія ";
+			ViewBag.Group = "Група: ІПЗ-22-1, ВУЗ: Хмельницький національний університет";
+			ViewBag.ProjectTopic = "Інтернет- магазин з продажу книг ";
+
+			return View();
+		}
+	}
+}
---
+++ lab1/Views/Home/Hello.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lab1.Views.Home
+{
+    public class HelloModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
